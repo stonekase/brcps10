606bf2b12eab4e7442fd70c3d151f0926be50aea
stonekase <cybrat@gmail.com>
2015-07-28 14:31:28 +0100

    this is the class responsible for processing and analysing the http requests menu

---
+++ brcps10/src/com/sourcard/helpers/analyseMenus.java
@@ -0,0 +1,15 @@
+package com.sourcard.helpers;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpSession;
+public class analyseMenus {
+	
+	public static void CustomerMenu(long msisdn,String menuName,HttpSession session,AsyncContext asyncCtx)
+	{
+		
+	}
+	public static void SubscriptionMenu(long Msisdn,String menuName,HttpSession session,AsyncContext asyncCtx)
+	{
+		
+	}
+}
